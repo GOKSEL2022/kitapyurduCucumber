6bb0e377b009295409ef2df55d05c14b44ea8242
Göksel Celik <gokselcelik5255@gmail.com>
2023-08-07 13:33:11 +0300

    07.08.2023

--- src/test/java/pages/HomePage.java
+++ src/test/java/pages/HomePage.java
@@ -5,6 +5,8 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import utilities.Driver;
 
+import java.util.List;
+
 public class HomePage {
     public HomePage(){
         PageFactory.initElements(Driver.getDriver(),this);
@@ -24,4 +26,13 @@ public class HomePage {
 
     @FindBy(xpath = "//*[@id='top-button']")
     public WebElement enUsteCikYonButonHome;
+
+    @FindBy(xpath = "//img[@class='swiper-lazy swiper-lazy-loaded']")
+    public List<WebElement> anasayfaKaydiriciResimlerHome;
+
+    @FindBy(xpath = "//*[@class='swiper-button-next common-sprite desktop-banner-next']")
+    public WebElement sagYonKaydiriciButonHome;
+
+    @FindBy(xpath = "//*[@class='swiper-button-prev common-sprite desktop-banner-prev']")
+    public WebElement solYonKaydiriciButonHome;
 }
--- src/test/java/stepdefinitions/TC04_KitapYurduHomeStepDefs.java
+++ src/test/java/stepdefinitions/TC04_KitapYurduHomeStepDefs.java
@@ -1,8 +1,14 @@
 package stepdefinitions;
 
 import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
+import org.junit.Assert;
+import org.openqa.selenium.WebElement;
 import pages.HomePage;
 import utilities.ReusableMethods;
+
+import java.util.List;
 
 public class TC04_KitapYurduHomeStepDefs {
     HomePage homePage=new HomePage();
@@ -20,4 +26,32 @@ public class TC04_KitapYurduHomeStepDefs {
     public void kullaniciBirSaniyeBekler() throws InterruptedException {
         Thread.sleep(1000);
     }
+
+    @Given("kullanici sag yon tusuna tiklar")
+    public void kullaniciSagYonTusunaTiklar() {
+        for (int i=1;i<homePage.anasayfaKaydiriciResimlerHome.size();i++){
+            homePage.sagYonKaydiriciButonHome.click();
+        }
+    }
+    @When("kullanici sag yon tusunun erisilebilir oldugunu dogrular")
+    public void kullaniciSagYonTusununErisilebilirOldugunuDogrular() {
+        Assert.assertTrue(homePage.sagYonKaydiriciButonHome.isEnabled());
+    }
+    @And("kullanici sol yon tusuna tiklar")
+    public void kullaniciSolYonTusunaTiklar() {
+        for (int i=1;i<homePage.anasayfaKaydiriciResimlerHome.size();i++) {
+            homePage.solYonKaydiriciButonHome.click();
+        }
+    }
+
+    @And("kullanici sol yon tusunun erisilebilir oldugunu dogrular")
+    public void kullaniciSolYonTusununErisilebilirOldugunuDogrular() {
+        Assert.assertTrue(homePage.solYonKaydiriciButonHome.isEnabled());
+    }
+
+    @And("kullanici anasayfada birden fazla resim oldugunu dogrular")
+    public void kullaniciAnasayfadaOnBesAdetKaydiriciOldugunuDogrular() {
+       Assert.assertTrue(homePage.anasayfaKaydiriciResimlerHome.size()>1);
+    }
+
 }
