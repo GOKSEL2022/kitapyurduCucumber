c9034c0af65005425dbd62a113678443a6276b6b
Göksel Celik <gokselcelik5255@gmail.com>
2023-09-23 00:03:50 +0300

    23.09.2023

---
+++ src/test/java/stepdefinitions/TC07_YayinevleriYayinlariYazarlarStepDefs.java
@@ -0,0 +1,35 @@
+package stepdefinitions;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
+
+public class TC07_YayinevleriYayinlariYazarlarStepDefs {
+    @Given("kullanici haftanin yayinevi basligina gelir")
+    public void kullaniciHaftaninYayineviBasliginaGelir() {
+    }
+
+    @When("kullanici secilen yayinevini goruntuler")
+    public void kullaniciSecilenYayineviniGoruntuler() {
+    }
+
+    @And("kullanici yapilan indirim miktarini goruntuler")
+    public void kullaniciYapilanIndirimMiktariniGoruntuler() {
+    }
+
+    @And("kullanici tumunu goster linkine tiklar")
+    public void kullaniciTumunuGosterLinkineTiklar() {
+    }
+
+    @And("kullanici acilan sayfada haftanin yayinevinin adini goruntuler")
+    public void kullaniciAcilanSayfadaHaftaninYayinevininAdiniGoruntuler() {
+    }
+
+    @And("kullanici cok satanlar icindeki ilk urune tiklar")
+    public void kullaniciCokSatanlarIcindekiIlkUruneTiklar() {
+    }
+
+    @When("kullanici acilan urunun ayni yayinevine ait oldugunu dogrular")
+    public void kullaniciAcilanUrununAyniYayinevineAitOldugunuDogrular() {
+    }
+}
